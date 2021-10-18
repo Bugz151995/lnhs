c05f725a9f11893683d825e9aefdf5b9884fb749
LHS08302021 <pj.alano12345@gmail.com>
2021-10-19 00:24:24 +0800

    last commit na tlga

--- app/Views/student/about.php
+++ app/Views/student/about.php
@@ -1,3 +1,354 @@
-<main class="container">
-  THIS IS THE ABOUT US PAGE
+<main>
+  <section class="about_bg py-5">
+    <div class="container">
+      <div class="row g-3 g-lg-5">
+        <div class="col-md-7 d-flex flex-column justify-content-center">
+          <h1 class="text-light text-center text-lg-start display-3 fw-bold mb-4">About Us</h1>
+          <h6 class="text-light lead text-center text-md-start mb-0">
+            The leading school in the K to 12 - Basic Education Program in the 4th District of Camarines Sur (Partido Area).
+          </h6>
+        </div>
+        <div class="col-md-5 d-flex justify-content-center">    
+          <img src="<?= site_url()?>assets/images/school_logo.png" alt="" class="img-fluid w-75">
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <section class="py-5 mt-5">
+    <div class="container py-5">
+      <div class="row g-5">
+        <div class="col-md-7 pe-lg-5 d-none d-sm-block">
+          <img src="<?= site_url()?>assets/images/bg2.jpg" alt="" class="img-fluid d-none d-sm-block">
+        </div>
+        <div class="col-md-5 d-flex flex-column justify-content-center">
+          <h1 class="display-3 text-center text-md-start fw-bold mb-4">Our Mission</h1>
+          <h6 class="text-center text-md-start mb-0 lead">
+            To provide better-quality instruction in the Basic Education Program that has a carry-over values for a God-fearing, environmental friendly and responsible learners in the pursuit for excellent education.
+          </h6>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <section class="py-5 my-5">
+    <div class="container">
+      <div class="row">
+        <div class="px-4 text-center border-bottom">
+          <h1 class="display-3 fw-bold mb-5">Meet Our Faculty</h1>
+          <div class="col-lg-6 mx-auto">
+          </div>
+          <div class="overflow-hidden" style="max-height: 30vh;">
+            <div class="container px-5">
+              <img src="<?= site_url()?>assets/images/3776134.jpg" class="img-fluid border rounded-3 shadow-lg mb-4" alt="Example image" width="700" height="500" loading="lazy">
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="py-5">        
+        <h1 class="text-center display-4 fw-bold mt-5">Grade 7 - Advisers</h1>
+        <div class="row my-5 py-5">
+          <div class="col-lg-4">
+            <div class="container d-flex flex-column align-items-center">
+              <img src="<?= site_url()?>assets/images/faculty/g-7_1.png" alt="" class="w-75 img-fluid rounded-circle">
+              <div class="lead text-center mt-3 fw-bold">EDZEL ABANILLA</div>
+              <div class="fst-italic">Grade 7- Ipil Adviser</div>
+            </div>
+          </div>
+          <div class="col-lg-4">
+            <div class="container d-flex flex-column align-items-center">
+              <img src="<?= site_url()?>assets/images/faculty/g-7_2.png" alt="" class="w-75 img-fluid rounded-circle">
+              <div class="lead text-center mt-3 fw-bold">CLARISSE AGRAVANTE</div>
+              <div class="fst-italic">Grade 7- Kamagong Adviser</div>
+            </div>
+          </div>
+          <div class="col-lg-4">
+            <div class="container d-flex flex-column align-items-center">
+              <img src="<?= site_url()?>assets/images/faculty/g-7_3.png" alt="" class="w-75 img-fluid rounded-circle">
+              <div class="lead text-center mt-3 fw-bold">MARISSA VALENCIA</div>
+              <div class="fst-italic">Grade 7- Apitong Adviser</div>
+            </div>
+          </div>
+        </div>
+
+        <div class="row justify-content-center">
+          <div class="col-lg-4">
+            <div class="container d-flex flex-column align-items-center">
+              <img src="<?= site_url()?>assets/images/faculty/g-7_4.png" alt="" class="w-75 img-fluid rounded-circle">
+              <div class="lead text-center mt-3 fw-bold">KATHY MORENO</div>
+              <div class="fst-italic">Grade 7- Yakal Adviser</div>
+            </div>
+          </div>
+          <div class="col-lg-4">
+            <div class="container d-flex flex-column align-items-center">
+              <img src="<?= site_url()?>assets/images/faculty/g-7_5.png" alt="" class="w-75 img-fluid rounded-circle">
+              <div class="lead text-center mt-3 fw-bold">JOHN MARK PRESTOZA</div>
+              <div class="fst-italic">Grade 7- Mulberry Adviser</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <section class="py-5 my-5">
+    <div class="py-3">        
+      <h1 class="text-center display-4 fw-bold mt-5">Grade 8 - Advisers</h1>
+      <div class="row my-5 py-5">
+        <div class="col-lg-4">
+          <div class="container d-flex flex-column align-items-center">
+            <img src="<?= site_url()?>assets/images/faculty/g-8_1.png" alt="" class="w-75 img-fluid rounded-circle">
+            <div class="lead text-center mt-3 fw-bold">JOAN BELGICA</div>
+            <div class="fst-italic">Grade 8- Mahogany Adviser</div>
+          </div>
+        </div>
+        <div class="col-lg-4">
+          <div class="container d-flex flex-column align-items-center">
+            <img src="<?= site_url()?>assets/images/faculty/g-8_2.png" alt="" class="w-75 img-fluid rounded-circle">
+            <div class="lead text-center mt-3 fw-bold">SHERILL OBIAS</div>
+            <div class="fst-italic">Grade 8- Pili Adviser</div>
+          </div>
+        </div>
+        <div class="col-lg-4">
+          <div class="container d-flex flex-column align-items-center">
+            <img src="<?= site_url()?>assets/images/faculty/g-8_3.png" alt="" class="w-75 img-fluid rounded-circle">
+            <div class="lead text-center mt-3 fw-bold">CATHERINE LORIAGA</div>
+            <div class="fst-italic">Grade 8- Pine Adviser</div>
+          </div>
+        </div>
+      </div>
+
+      <div class="row justify-content-center">
+        <div class="col-lg-4">
+          <div class="container d-flex flex-column align-items-center">
+            <img src="<?= site_url()?>assets/images/faculty/g-8_4.png" alt="" class="w-75 img-fluid rounded-circle">
+            <div class="lead text-center mt-3 fw-bold">JOAN GARCIA</div>
+            <div class="fst-italic">Grade 8- Tangueli Adviser</div>
+          </div>
+        </div>
+        <div class="col-lg-4">
+          <div class="container d-flex flex-column align-items-center">
+            <img src="<?= site_url()?>assets/images/faculty/g-8_5.png" alt="" class="w-75 img-fluid rounded-circle">
+            <div class="lead text-center mt-3 fw-bold">SHARMAGNE ROSERO</div>
+            <div class="fst-italic">Grade 8- Talisay Adviser</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <section class="py-5 my-5">
+    <div class="py-3">        
+      <h1 class="text-center display-4 fw-bold mt-5">Grade 9 - Advisers</h1>
+      <div class="row my-5 py-5">
+        <div class="col-lg-4">
+          <div class="container d-flex flex-column align-items-center">
+            <img src="<?= site_url()?>assets/images/faculty/g-9_1.png" alt="" class="w-75 img-fluid rounded-circle">
+            <div class="lead text-center mt-3 fw-bold">MARILOU CAJADE</div>
+            <div class="fst-italic">Grade 9- Guiho Adviser</div>
+          </div>
+        </div>
+        <div class="col-lg-4">
+          <div class="container d-flex flex-column align-items-center">
+            <img src="<?= site_url()?>assets/images/faculty/g-9_2.png" alt="" class="w-75 img-fluid rounded-circle">
+            <div class="lead text-center mt-3 fw-bold">JOEL PERICO</div>
+            <div class="fst-italic">Grade 9- Lauan Adviser</div>
+          </div>
+        </div>
+        <div class="col-lg-4">
+          <div class="container d-flex flex-column align-items-center">
+            <img src="<?= site_url()?>assets/images/faculty/g-9_3.png" alt="" class="w-75 img-fluid rounded-circle">
+            <div class="lead text-center mt-3 fw-bold">MARICEL FLORES</div>
+            <div class="fst-italic">Grade 9- Dao Adviser</div>
+          </div>
+        </div>
+      </div>
+
+      <div class="row justify-content-center">
+        <div class="col-lg-4">
+          <div class="container d-flex flex-column align-items-center">
+            <img src="<?= site_url()?>assets/images/faculty/g-9_4.png" alt="" class="w-75 img-fluid rounded-circle">
+            <div class="lead text-center mt-3 fw-bold">DAN JARL ALBERT VERGARA</div>
+            <div class="fst-italic">Grade 9- Batikuling Adviser</div>
+          </div>
+        </div>
+        <div class="col-lg-4">
+          <div class="container d-flex flex-column align-items-center">
+            <img src="<?= site_url()?>assets/images/faculty/g-9_5.png" alt="" class="w-75 img-fluid rounded-circle">
+            <div class="lead text-center mt-3 fw-bold">JOHN JOSEPH NIÑO BRIONES</div>
+            <div class="fst-italic">Grade 9- Madera Adviser</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <section class="py-5 my-5">
+    <div class="py-3">        
+      <h1 class="text-center display-4 fw-bold mt-5">Grade 10 - Advisers</h1>
+      <div class="row my-5 py-5">
+        <div class="col-lg-4">
+          <div class="container d-flex flex-column align-items-center">
+            <img src="<?= site_url()?>assets/images/faculty/g-10_1.png" alt="" class="w-75 img-fluid rounded-circle">
+            <div class="lead text-center mt-3 fw-bold"></div>
+            <div class="fst-italic">Grade 10- Barayong Adviser</div>
+          </div>
+        </div>
+        <div class="col-lg-4">
+          <div class="container d-flex flex-column align-items-center">
+            <img src="<?= site_url()?>assets/images/faculty/g-10_2.png" alt="" class="w-75 img-fluid rounded-circle">
+            <div class="lead text-center mt-3 fw-bold"></div>
+            <div class="fst-italic">Grade 10- Narra Adviser</div>
+          </div>
+        </div>
+        <div class="col-lg-4">
+          <div class="container d-flex flex-column align-items-center">
+            <img src="<?= site_url()?>assets/images/faculty/g-10_3.png" alt="" class="w-75 img-fluid rounded-circle">
+            <div class="lead text-center mt-3 fw-bold"></div>
+            <div class="fst-italic">Grade 10- Dita Adviser</div>
+          </div>
+        </div>
+      </div>
+
+      <div class="row justify-content-center">
+        <div class="col-lg-4">
+          <div class="container d-flex flex-column align-items-center">
+            <img src="<?= site_url()?>assets/images/faculty/g-10_4.png" alt="" class="w-75 img-fluid rounded-circle">
+            <div class="lead text-center mt-3 fw-bold"></div>
+            <div class="fst-italic">Grade 10- Acacia Adviser</div>
+          </div>
+        </div>
+        <div class="col-lg-4">
+          <div class="container d-flex flex-column align-items-center">
+            <img src="<?= site_url()?>assets/images/faculty/g-10_5.png" alt="" class="w-75 img-fluid rounded-circle">
+            <div class="lead text-center mt-3 fw-bold"></div>
+            <div class="fst-italic">Grade 10- Molave Adviser</div>
+          </div>
+        </div>
+        <div class="col-lg-4">
+          <div class="container d-flex flex-column align-items-center">
+            <img src="<?= site_url()?>assets/images/faculty/g-10_6.png" alt="" class="w-75 img-fluid rounded-circle">
+            <div class="lead text-center mt-3 fw-bold"></div>
+            <div class="fst-italic">Grade 10- Gmelina Adviser</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <section class="py-5 my-5">
+    <div class="py-3">        
+      <h1 class="text-center display-4 fw-bold mt-5">Grade 11 - Advisers</h1>
+      <div class="row my-5 py-5">
+        <div class="col-lg-4">
+          <div class="container d-flex flex-column align-items-center">
+            <img src="<?= site_url()?>assets/images/faculty/g-11_1.png" alt="" class="w-75 img-fluid rounded-circle">
+            <div class="lead text-center mt-3 fw-bold">SHERYL ENCISO</div>
+            <div class="fst-italic">Grade 11- GAS A Adviser</div>
+          </div>
+        </div>
+        <div class="col-lg-4">
+          <div class="container d-flex flex-column align-items-center">
+            <img src="<?= site_url()?>assets/images/faculty/g-11_2.png" alt="" class="w-75 img-fluid rounded-circle">
+            <div class="lead text-center mt-3 fw-bold">ARACELI  BARREDA</div>
+            <div class="fst-italic">Grade 11- ABM Adviser</div>
+          </div>
+        </div>
+        <div class="col-lg-4">
+          <div class="container d-flex flex-column align-items-center">
+            <img src="<?= site_url()?>assets/images/faculty/g-11_3.png" alt="" class="w-75 img-fluid rounded-circle">
+            <div class="lead text-center mt-3 fw-bold">CARLO PARZA</div>
+            <div class="fst-italic">Grade 11- CSS, GAS C Adviser</div>
+          </div>
+        </div>
+      </div>
+
+      <div class="row justify-content-center">
+        <div class="col-lg-4">
+          <div class="container d-flex flex-column align-items-center">
+            <img src="<?= site_url()?>assets/images/faculty/g-11_4.png" alt="" class="w-75 img-fluid rounded-circle">
+            <div class="lead text-center mt-3 fw-bold">JO ANN REMODO</div>
+            <div class="fst-italic">Grade 11- GAS B Adviser</div>
+          </div>
+        </div>
+        <div class="col-lg-4">
+          <div class="container d-flex flex-column align-items-center">
+            <img src="<?= site_url()?>assets/images/faculty/g-11_5.png" alt="" class="w-75 img-fluid rounded-circle">
+            <div class="lead text-center mt-3 fw-bold">ALBERT  PARCIA</div>
+            <div class="fst-italic">Grade 11- EIM Adviser</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <section class="py-5 my-5">
+    <div class="py-3">        
+      <h1 class="text-center display-4 fw-bold mt-5">Grade 12 - Advisers</h1>
+      <div class="row my-5 py-5">
+        <div class="col-lg-4">
+          <div class="container d-flex flex-column align-items-center">
+            <img src="<?= site_url()?>assets/images/faculty/g-12_1.png" alt="" class="w-75 img-fluid rounded-circle">
+            <div class="lead text-center mt-3 fw-bold">RONNIE CORONO</div>
+            <div class="fst-italic">Grade 12- GAS A Adviser</div>
+          </div>
+        </div>
+        <div class="col-lg-4">
+          <div class="container d-flex flex-column align-items-center">
+            <img src="<?= site_url()?>assets/images/faculty/g-12_2.png" alt="" class="w-75 img-fluid rounded-circle">
+            <div class="lead text-center mt-3 fw-bold">JEENEZES REVELS</div>
+            <div class="fst-italic">Grade 12- ABM, GAS B Adviser</div>
+          </div>
+        </div>
+        <div class="col-lg-4">
+          <div class="container d-flex flex-column align-items-center">
+            <img src="<?= site_url()?>assets/images/faculty/g-12_3.png" alt="" class="w-75 img-fluid rounded-circle">
+            <div class="lead text-center mt-3 fw-bold">CHED AUGUSTUS PACHO</div>
+            <div class="fst-italic">Grade 12- CSS Adviser</div>
+          </div>
+        </div>
+      </div>
+
+      <div class="row justify-content-center">
+        <div class="col-lg-4">
+          <div class="container d-flex flex-column align-items-center">
+            <img src="<?= site_url()?>assets/images/faculty/g-12_4.png" alt="" class="w-75 img-fluid rounded-circle">
+            <div class="lead text-center mt-3 fw-bold">ALMA ORTINERO</div>
+            <div class="fst-italic">Grade 12- HE, 12 - HE Adviser</div>
+          </div>
+        </div>
+        <div class="col-lg-4">
+          <div class="container d-flex flex-column align-items-center">
+            <img src="<?= site_url()?>assets/images/faculty/g-12_5.png" alt="" class="w-75 img-fluid rounded-circle">
+            <div class="lead text-center mt-3 fw-bold">HERMY PASIONA</div>
+            <div class="fst-italic">Grade 12- EIM Adviser</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <section class="py-5 my-5">
+    <div class="py-3">        
+      <h1 class="text-center display-4 fw-bold mt-5">Part Time Teachers</h1>
+
+      <div class="row justify-content-center my-5 py-5">
+        <div class="col-lg-4">
+          <div class="container d-flex flex-column align-items-center">
+            <img src="<?= site_url()?>assets/images/faculty/g-p_1.png" alt="" class="w-75 img-fluid rounded-circle">
+            <div class="lead text-center mt-3 fw-bold">ROMEO ALVAREZ</div>
+            <div class="fst-italic">SHS Instructor</div>
+          </div>
+        </div>
+        <div class="col-lg-4">
+          <div class="container d-flex flex-column align-items-center">
+            <img src="<?= site_url()?>assets/images/faculty/g-p_2.png" alt="" class="w-75 img-fluid rounded-circle">
+            <div class="lead text-center mt-3 fw-bold">EDUARDO DE HITTA III</div>
+            <div class="fst-italic">SHS Instructor</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
 </main>

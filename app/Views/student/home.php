0603e5bd181e828a212ca430f1511c16f44fe0da
LHS08302021 <pj.alano12345@gmail.com>
2021-11-22 14:21:13 +0800

    fix on esc voucher management

--- app/Views/student/home.php
+++ app/Views/student/home.php
@@ -1,5 +1,5 @@
 <main class="container">
-  <section class="row py-4 py-sm-0 px-2">
+  <section class="row py-5 my-5 py-sm-0 px-2">
     <div class="col-sm-7 text-center text-sm-start d-flex justify-content-center flex-column">
       <h4>Enrollment Made Easy</h4>
       <p>Lagonoy High School now offers online enrollment for incoming Senior High School Students</p>
@@ -14,15 +14,12 @@
     </div>
   </section>
 
-  <section class="row row-cols-sm-3 g-sm-5 mx-4">
+  <section class="row row-cols-sm-3 g-sm-5 mx-4 py-5 my-5">
     <div class="col text-center text-sm-start">
       <div class="bg-secondary rounded p-4 text-light">
         <i class="fas fa-list-alt fa-fw fa-4x"></i>
         <h5 class="py-3">Online Enrollment</h5>
         <p>Our School offers enrollment on the go. Enroll wherever you are.</p>
-        <a href="#" class="btn btn-dark">
-          <i class="fas fa-chevron-right"></i> Read More
-        </a>
       </div>
     </div>
 
@@ -31,9 +28,6 @@
         <i class="fas fa-ticket-alt fa-fw fa-4x"></i>
         <h5 class="py-3">ESC Voucher</h5>
         <p>Our School also offers processing of ESC Vouchers online.</p>
-        <a href="#" class="btn btn-secondary">
-          <i class="fas fa-chevron-right"></i> Read More
-        </a>
       </div>
     </div>
 
@@ -42,14 +36,11 @@
         <i class="fas fa-users fa-fw fa-4x"></i>
         <h5 class="py-3">Faculty Search</h5>
         <p>Our School allows transparency with our faculty and staff.</p>
-        <a href="#" class="btn btn-dark">
-          <i class="fas fa-chevron-right"></i> Read More
-        </a>
       </div>
     </div>
   </section>
 
-  <section class="row py-5">
+  <section class="row py-5 my-5">
     <div class="col-sm-5">
       <img src="<?= site_url()?>assets/images/study_2.jpg" alt="" class="img-fluid">
     </div>
@@ -57,7 +48,7 @@
       <h4>ESC Voucher Registration</h4>
       <p>Lagonoy High School also offers online processing of eligible ESC grantees, it is made easy and user-friendly.</p>
       <div class="container p-0">
-        <a href="<?= site_url()?>esc_registration" class="btn btn-primary">
+        <a href="<?= site_url()?>esc" class="btn btn-primary">
           <i class="fas fa-chevron-right"></i> Register Now
         </a>
       </div>
